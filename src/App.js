8202c5342885ee1ff7e9f9ab4e83f141cbe23c54
Sheetal Varsani <sheetalvarsani@hotmail.co.uk>
2024-01-10 14:21:42 +0000

    Moved Footer code from App.js into its own Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 class App extends React.Component {
   render() {
@@ -151,41 +152,7 @@ class App extends React.Component {
           </div>
         </main>
 
-        <footer className="scene">
-          <article className="content">
-            <div id="socialmedia">
-              <ul className="group">
-                <li>
-                  <a href="https://twitter.com">
-                    <img
-                      className="icon"
-                      src="https://landonhotel.com/images/socialmedia/twitter.png"
-                      alt="icon for twitter"
-                    />
-                  </a>
-                </li>
-                <li>
-                  <a href="http://www.facebook.com">
-                    <img
-                      className="icon"
-                      src="https://landonhotel.com/images/socialmedia/facebook.png"
-                      alt="icon for facebook"
-                    />
-                  </a>
-                </li>
-                <li>
-                  <a href="http://www.youtube.com">
-                    <img
-                      className="icon"
-                      src="https://landonhotel.com/images/socialmedia/youtube.png"
-                      alt="icon for youtube"
-                    />
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </article>
-        </footer>
+        <Footer />
       </div>
     );
   }
