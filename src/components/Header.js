7689694d3a08fac86b4904b0ffc89b74a710ce41
Sheetal Varsani <sheetalvarsani@hotmail.co.uk>
2024-01-17 16:34:18 +0000

    Added useState and useEffect hooks to Header.js to request data from API Gateway for menu links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,23 @@
-import React from "react";
-import menuLinksData from './data/menu_links.json'
+import React, { useState, useEffect } from "react";
+// import menuLinksData from './data/menu_links.json' - Commented out so we're not using the mock json data
 
 const Header = () => {
+  const [menuLinksData, setMenuLinksData] = useState([]);
+
+  const loadMenuLinksData = async() => {
+    // Query the API Gateway
+    const resp = await fetch('https://3yys1qu4o9.execute-api.eu-north-1.amazonaws.com/Production/menu_links') // resp for response
+    let jsonData = await resp.json();
+
+    // Assign response data to our state variable
+    setMenuLinksData(jsonData);
+  }
+
+  useEffect(() => {
+    // Load the menu link data from the API gGteway
+    loadMenuLinksData();
+  }, []);
+
   return (
     <header id="intro">
       <article className="fullheight">
